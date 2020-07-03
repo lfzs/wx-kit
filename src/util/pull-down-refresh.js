067f7b5b0106f81ac82688171eb5cd3552dafc59
jstsoso <jycomputer_417@163.com>
2020-07-03 10:15:48 +0800

    fix: canvas reject

--- src/util/pull-down-refresh.js
+++ src/util/pull-down-refresh.js
@@ -5,8 +5,8 @@ export default function(target, name, descriptor) {
     wx.vibrateShort()
     Promise.resolve(func.apply(this, args))
       .then(() => wx.stopPullDownRefresh())
-      .catch(e => {
-        console.warn(e) // eslint-disable-line no-console
+      .catch(error => {
+        console.warn(error) // eslint-disable-line no-console
         wx.stopPullDownRefresh()
         wx.showToast({ title: '请求失败', icon: 'none' })
       })
